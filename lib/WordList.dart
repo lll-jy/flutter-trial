b738ac5ee393fdd2f682840b6e89a0ea61a7cd34
lll-jy <E0376997@u.nus.edu>
2020-12-23 09:37:52 +0800

    Add color feature for list

--- lib/WordList.dart
+++ lib/WordList.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// Adapted from https://api.flutter.dev/flutter/material/DataTable-class.html
+
 class WordList extends StatefulWidget {
   WordList({Key key}) : super(key: key);
 
@@ -11,6 +13,7 @@ class _WordListState extends State<WordList> {
   static const int num = 10;
   List<String> words = ['Apple', 'Banana', 'Cat', 'Dog', 'Eat', 'Frank',
     'Grape', 'Hello', 'I', 'Jacket'];
+  List<bool> selected = List<bool>.generate(num, (index) => false);
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +24,20 @@ class _WordListState extends State<WordList> {
               DataColumn(label: Text('Words'))
             ],
             rows: List<DataRow>.generate(
-                num,
-                    (index) => DataRow(
+                num, (index) => DataRow(
+                    color: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.selected))
+                        return Theme.of(context).colorScheme.primary.withOpacity(0.08);
+                      if (index % 2 == 0) return Colors.grey.withOpacity(0.3);
+                      return null; // Use default value for other states and odd rows.
+                    }),
                     cells: [DataCell(Text(words[index]))],
-                    selected: false,
+                    selected: selected[index],
                     onSelectChanged: (bool value) {
-
+                      setState(() {
+                        selected[index] = value;
+                      });
                     }
                 ))
         )
