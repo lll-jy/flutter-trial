100ef2b8f991394e6fe91de375a97601b17b8e98
lll-jy <E0376997@u.nus.edu>
2020-12-24 11:03:11 +0800

    Form basics

--- lib/components/NewWordPage.dart
+++ lib/components/NewWordPage.dart
@@ -12,6 +12,14 @@ class NewWordPage extends StatefulWidget {
 }
 
 class _NewWordPageState extends State<NewWordPage> {
+  String word;
+  Speech speech = Speech.n;
+  String glossary;
+  String example;
+  DateTime createdAt;
+  DateTime lastReviewAt;
+  List<Category> categories;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +27,72 @@ class _NewWordPageState extends State<NewWordPage> {
           title: Text('Add new word')
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              widget.words.map((e) => e.word).toString()
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'New word saved!');
+        child: FractionallySizedBox(
+          widthFactor: 0.9,
+          child: Column(
+            children: <Widget>[
+              Text(''), // Placeholder
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Word (original form)'
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    word = value;
+                  });
                 },
-                child: Text('Submit')
-            )
-          ],
+              ),
+              FractionallySizedBox(
+                widthFactor: 1,
+                child: DropdownButton<String>(
+                  value: Word.speechToStr(speech),
+                  hint: Text('Select a speech of word'),
+                  isExpanded: true,
+                  onChanged: (String newSpeech) {
+                    setState(() {
+                      speech = Word.strToSpeech(newSpeech);
+                    });
+                  },
+                  items: <String>['n.', 'v.', 'adj.', 'adv.', 'article', 'pron.',
+                    'prep.', 'conj.', 'interj.']
+                      .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e)
+                  )).toList()
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Glossary'
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    glossary = value;
+                  });
+                },
+                maxLines: 5,
+                minLines: 1,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Example'
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    example = value;
+                  });
+                },
+                maxLines: 5,
+                minLines: 1,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'New word saved!');
+                  },
+                  child: Text('Submit')
+              )
+            ],
+          )
         ),
       ),
     );
