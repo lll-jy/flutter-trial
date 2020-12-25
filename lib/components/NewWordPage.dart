cb1251d51121deaf53e2ce690f2abf91411b139b
lll-jy <E0376997@u.nus.edu>
2020-12-25 19:03:01 +0800

    Add validator to avoid repetition

--- lib/components/NewWordPage.dart
+++ lib/components/NewWordPage.dart
@@ -28,6 +28,7 @@ class _NewWordPageState extends State<NewWordPage> {
     Category.SAT: false,
     Category.TOEFL: false
   };
+  final _formKey = GlobalKey<FormState>();
 
   List<Category> getCategories() {
     List<Category> res = [];
@@ -48,147 +49,160 @@ class _NewWordPageState extends State<NewWordPage> {
       body: Center(
         child: FractionallySizedBox(
           widthFactor: 0.9,
-          child: Column(
-            children: <Widget>[
-              Text(''), // Placeholder
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Word (original form)'
-                ),
-                onChanged: (String value) {
-                  setState(() {
-                    word = value;
-                  });
-                },
-              ),
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: DropdownButton<String>(
-                  value: Word.speechToStr(speech),
-                  hint: Text('Select a speech of word'),
-                  isExpanded: true,
-                  onChanged: (String newSpeech) {
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text(''), // Placeholder
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Word (original form)'
+                  ),
+                  onChanged: (String value) {
                     setState(() {
-                      speech = Word.strToSpeech(newSpeech);
+                      word = value;
                     });
                   },
-                  items: <String>['n.', 'v.', 'adj.', 'adv.', 'article', 'pron.',
-                    'prep.', 'conj.', 'interj.']
-                      .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
-                      value: e,
-                      child: Text(e)
-                  )).toList()
+                  validator: (value) {
+                    for (Word w in widget.words) {
+                      if (w.word == value) {
+                        return 'This word already exists in the list.';
+                      }
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Glossary'
-                ),
-                onChanged: (String value) {
-                  setState(() {
-                    glossary = value;
-                  });
-                },
-                maxLines: 5,
-                minLines: 1,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Example'
-                ),
-                onChanged: (String value) {
-                  setState(() {
-                    example = value;
-                  });
-                },
-                maxLines: 5,
-                minLines: 1,
-              ),
-              Text(''), // placeholder
-              FractionallySizedBox(
-                widthFactor: 1,
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Categories:',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontSize: 15
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: DropdownButton<String>(
+                    value: Word.speechToStr(speech),
+                    hint: Text('Select a speech of word'),
+                    isExpanded: true,
+                    onChanged: (String newSpeech) {
+                      setState(() {
+                        speech = Word.strToSpeech(newSpeech);
+                      });
+                    },
+                    items: <String>['n.', 'v.', 'adj.', 'adv.', 'article', 'pron.',
+                      'prep.', 'conj.', 'interj.']
+                        .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(e)
+                    )).toList()
                   ),
                 ),
-              ),
-              CategoryCheckbox(
-                label: 'CET4',
-                value: categories[Category.CET4],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.CET4] = v;
-                  });
-                },
-              ),
-              CategoryCheckbox(
-                label: 'CET8',
-                value: categories[Category.CET8],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.CET8] = v;
-                  });
-                },
-              ),
-              CategoryCheckbox(
-                label: 'GRE',
-                value: categories[Category.GRE],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.GRE] = v;
-                  });
-                },
-              ),
-              CategoryCheckbox(
-                label: 'SAT',
-                value: categories[Category.SAT],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.SAT] = v;
-                  });
-                },
-              ),
-              CategoryCheckbox(
-                label: 'TOEFL',
-                value: categories[Category.TOEFL],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.TOEFL] = v;
-                  });
-                },
-              ),
-              CategoryCheckbox(
-                label: 'GMAT',
-                value: categories[Category.GMAT],
-                onChanged: (v) {
-                  setState(() {
-                    categories[Category.GMAT] = v;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  List<Word> words = widget.words;
-                  words.add(Word(
-                      word: word,
-                      speech: speech,
-                      glossary: glossary,
-                      example: example,
-                      createdAt: DateTime.now(),
-                      lastReviewAt: DateTime.now(),
-                      expectedInterval: 1,
-                      categories: getCategories()
-                  ));
-                  Storage.update(words);
-                  Navigator.pop(context, 'New word saved!');
-                },
-                child: Text('Submit')
-              )
-            ],
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Glossary'
+                  ),
+                  onChanged: (String value) {
+                    setState(() {
+                      glossary = value;
+                    });
+                  },
+                  maxLines: 5,
+                  minLines: 1,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Example'
+                  ),
+                  onChanged: (String value) {
+                    setState(() {
+                      example = value;
+                    });
+                  },
+                  maxLines: 5,
+                  minLines: 1,
+                ),
+                Text(''), // placeholder
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Categories:',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 15
+                    ),
+                  ),
+                ),
+                CategoryCheckbox(
+                  label: 'CET4',
+                  value: categories[Category.CET4],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.CET4] = v;
+                    });
+                  },
+                ),
+                CategoryCheckbox(
+                  label: 'CET8',
+                  value: categories[Category.CET8],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.CET8] = v;
+                    });
+                  },
+                ),
+                CategoryCheckbox(
+                  label: 'GRE',
+                  value: categories[Category.GRE],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.GRE] = v;
+                    });
+                  },
+                ),
+                CategoryCheckbox(
+                  label: 'SAT',
+                  value: categories[Category.SAT],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.SAT] = v;
+                    });
+                  },
+                ),
+                CategoryCheckbox(
+                  label: 'TOEFL',
+                  value: categories[Category.TOEFL],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.TOEFL] = v;
+                    });
+                  },
+                ),
+                CategoryCheckbox(
+                  label: 'GMAT',
+                  value: categories[Category.GMAT],
+                  onChanged: (v) {
+                    setState(() {
+                      categories[Category.GMAT] = v;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      List<Word> words = widget.words;
+                      words.add(Word(
+                          word: word,
+                          speech: speech,
+                          glossary: glossary,
+                          example: example,
+                          createdAt: DateTime.now(),
+                          lastReviewAt: DateTime.now(),
+                          expectedInterval: 1,
+                          categories: getCategories()
+                      ));
+                      Storage.update(words);
+                      Navigator.pop(context, 'New word saved!');
+                    }
+                  },
+                  child: Text('Submit')
+                )
+              ],
+            )
           )
         ),
       ),
